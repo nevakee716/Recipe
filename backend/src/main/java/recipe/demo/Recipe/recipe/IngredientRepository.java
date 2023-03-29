9bb19d3344353e8d77288035e63a4586efc83b3c
nevakee716 <nevakee716@gmail.com>
2023-03-30 00:22:40 +0200

    now update the postgres DB instead of wipe it

--- backend/src/main/java/recipe/demo/Recipe/recipe/IngredientRepository.java
+++ backend/src/main/java/recipe/demo/Recipe/recipe/IngredientRepository.java
@@ -3,6 +3,9 @@ package recipe.demo.Recipe.recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
+    List<Ingredient> findByName(String name);
 }
