1c3ddbad3b8f7e7ffc5a051afa65e4000da266fe
nevakee716 <nevakee716@gmail.com>
2023-03-30 01:21:44 +0200

    add find by name

--- backend/src/main/java/recipe/demo/Recipe/recipe/RecipeRepository.java
+++ backend/src/main/java/recipe/demo/Recipe/recipe/RecipeRepository.java
@@ -1,9 +1,14 @@
 package recipe.demo.Recipe.recipe;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface RecipeRepository extends JpaRepository<Recipe,Long> {
+    @Query(value = "SELECT * FROM recipe r WHERE LOWER(r.name) = LOWER(:name)", nativeQuery = true)
+    List<Recipe> findByName(String name);
 }
 
