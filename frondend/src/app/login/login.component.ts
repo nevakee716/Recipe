89b8af72fc5d358dcf626a604843e3b8b7d04d60
nevakee716 <nevakee716@gmail.com>
2023-04-01 21:14:59 +0200

    improve look n feel and display error message

--- frondend/src/app/login/login.component.ts
+++ frondend/src/app/login/login.component.ts
@@ -2,26 +2,30 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
-
+import { FormGroup, FormControl } from '@angular/forms';
+import { lastValueFrom } from 'rxjs';
 @Component({
     selector: 'app-login',
     templateUrl: './login.component.html',
     styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-    email: string = '';
-    password: string = '';
-
+    form: FormGroup = new FormGroup({
+        email: new FormControl(''),
+        password: new FormControl(''),
+    });
+    error: any = undefined;
     constructor(private authService: AuthService, private router: Router) {}
 
-    onSubmit() {
-        this.authService.login({ email: this.email, password: this.password }).subscribe(
-            () => {
+    async submit() {
+        if (this.form.valid) {
+            try {
+                await lastValueFrom(this.authService.login({ email: this.form.value.email, password: this.form.value.password }));
                 this.router.navigate(['/recipes']);
-            },
-            (error: any) => {
+            } catch (error) {
+                this.error = error;
                 console.log('Error:', error);
             }
-        );
+        }
     }
 }
