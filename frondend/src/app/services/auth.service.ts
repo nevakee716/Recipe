510efec191b5b97d581a43994bb143813ae2e197
nevakee716 <nevakee716@gmail.com>
2023-04-02 00:27:22 +0200

    logout method

--- frondend/src/app/services/auth.service.ts
+++ frondend/src/app/services/auth.service.ts
@@ -5,6 +5,7 @@ import { JwtHelperService } from '@auth0/angular-jwt';
 import { environment } from '../../environments/environment';
 import { User } from '../models/user';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
     providedIn: 'root',
@@ -24,8 +25,10 @@ export class AuthService {
         return this.http.get<User>(`${environment.authUrl}/userinfo`);
     }
 
-    logout() {
+    async logout() {
+        let r = await lastValueFrom(this.http.get<{ token: string }>(`${environment.authUrl}/logout`));
         localStorage.removeItem('token');
+        this.router.navigate(['/login']);
     }
 
     isAuthenticated(): boolean {
