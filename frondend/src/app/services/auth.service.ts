930b82b825287e23211d98a796ad3b3dd578385d
nevakee716 <nevakee716@gmail.com>
2023-04-02 00:27:36 +0200

    getuserdetail

--- frondend/src/app/services/auth.service.ts
+++ frondend/src/app/services/auth.service.ts
@@ -1,17 +1,33 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { tap } from 'rxjs/operators';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { environment } from '../../environments/environment';
 import { User } from '../models/user';
-import { Observable } from 'rxjs';
+import { lastValueFrom, Observable, Subject } from 'rxjs';
+import { switchMap, tap, map } from 'rxjs/operators';
 import { Router } from '@angular/router';
 
 @Injectable({
     providedIn: 'root',
 })
 export class AuthService {
-    constructor(private http: HttpClient, public jwtHelper: JwtHelperService) {}
+    private readonly getUserDetailSubject = new Subject<void>();
+    constructor(private http: HttpClient, public jwtHelper: JwtHelperService, private router: Router) {}
+
+    getUserDetail(): Observable<User> {
+        return this.getUserDetailSubject.pipe(
+            switchMap(() => this.http.get<User>(`${environment.authUrl}/userinfo`)),
+            map((u) => {
+                u.username = u.firstname ? `${u.firstname} ${u.lastname}` : u.email;
+                return u;
+            }),
+            tap(() => console.log('GET UserInfo request triggered'))
+        );
+    }
+
+    triggerRefreshUserInfo(): void {
+        this.getUserDetailSubject.next();
+    }
 
     login(credentials: { email: string; password: string }) {
         return this.http.post<{ token: string }>(`${environment.authUrl}/authenticate`, credentials).pipe(
@@ -19,10 +35,6 @@ export class AuthService {
                 localStorage.setItem('token', res.token);
             })
         );
-    }
-
-    getUserDetail(): Observable<User> {
-        return this.http.get<User>(`${environment.authUrl}/userinfo`);
     }
 
     async logout() {
