e8f4ca73f54074f9cb0f1377fdc6233a682991d6
nevakee716 <nevakee716@gmail.com>
2023-04-04 17:20:34 +0200

    using option for configuration (need to be sorting somewhere in xml)

--- backend/src/main/java/recipe/demo/Recipe/recipe/RecipeConfiguration.java
+++ backend/src/main/java/recipe/demo/Recipe/recipe/RecipeConfiguration.java
@@ -21,79 +21,91 @@ public class RecipeConfiguration {
     CommandLineRunner commandLineRunner(AuthenticationService authService, UserRepository userRepository, RecipeRepository recipeRepository, IngredientRepository ingredientRepository, KeywordRepository keywordRepository) {
 
         return args -> {
+            Boolean initUser= true;
+            Boolean initKeywords= true;
+            Boolean initIngredients = true;
+            Boolean initRecipe = false;
 
-            UserService userService = new UserService(userRepository);
-            // Create Admin
-            User adminUser;
-            Optional<User> ru = userRepository.findByEmail("admin");
 
-            if(ru.isEmpty()) {
-                adminUser = new User(1,"admin","admin","admin","admin", Role.ADMIN,new ArrayList<>(),new ArrayList<>(),new ArrayList<>());
-                authService.register(adminUser);
-            } else {
-                adminUser = ru.get();
-            }
 
-            // Check or Create Default Keyword
+
+
             List<Keyword> keywordsList = new ArrayList<>();
-            List.of("Plat en sauce", "Chaud", "Viande", "Lactose", "Froid","Grillade").forEach(keywordName -> {
-                List<Keyword> r = keywordRepository.findByName(keywordName);
-                if (r.size() > 0) keywordsList.add(r.get(0));
-                else {
-                    Keyword newKeyword = new Keyword();
-                    newKeyword.setName(keywordName);
-                    keywordRepository.save(newKeyword);
-                    keywordsList.add(newKeyword);
-                }
-            });
+            if(initKeywords) {
+                // Check or Create Default Keyword
 
-            // Check or Create Default Ingredients
+                List.of("Plat en sauce", "Chaud", "Viande", "Lactose", "Froid", "Grillade").forEach(keywordName -> {
+                    List<Keyword> r = keywordRepository.findByName(keywordName);
+                    if (r.size() > 0) keywordsList.add(r.get(0));
+                    else {
+                        Keyword newKeyword = new Keyword();
+                        newKeyword.setName(keywordName);
+                        keywordRepository.save(newKeyword);
+                        keywordsList.add(newKeyword);
+                    }
+                });
+            }
+
             List<Ingredient> ingredientsList = new ArrayList<>();
-            List.of("Boeuf", "Navet", "Carotte", "Crème", "Veau").forEach(ing -> {
-                List<Ingredient> r = ingredientRepository.findByName(ing);
-                if (r.size() > 0) ingredientsList.add(r.get(0));
-                else {
-                    Ingredient newIngredient = new Ingredient(ing);
-                    ingredientRepository.save(newIngredient);
-                    ingredientsList.add(newIngredient);
+            if(initIngredients) {
+                // Check or Create Default Ingredients
+
+                List.of("Boeuf", "Navet", "Carotte", "Crème", "Veau").forEach(ing -> {
+                    List<Ingredient> r = ingredientRepository.findByName(ing);
+                    if (r.size() > 0) ingredientsList.add(r.get(0));
+                    else {
+                        Ingredient newIngredient = new Ingredient(ing);
+                        ingredientRepository.save(newIngredient);
+                        ingredientsList.add(newIngredient);
+                    }
+                });
+            }
+
+            if(initUser) {
+                User adminUser;
+                UserService userService = new UserService(userRepository);
+                // Create Admin
+                Optional<User> ru = userRepository.findByEmail("admin");
+
+                if (ru.isEmpty()) {
+                    adminUser = new User(1, "admin", "admin", "admin", "admin", Role.ADMIN, new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
+                    authService.register(adminUser);
+                } else {
+                    adminUser = ru.get();
                 }
-            });
 
+                if(initKeywords && initIngredients && initRecipe) {
+                    List<Recipe> r = new ArrayList<>();
 
-            List<Recipe> r = new ArrayList<>();
+                    // Create Recipe 1
+                    r = recipeRepository.findByName("Blanquette de veau");
+                    if (r.size() == 0) {
+                        Recipe blanquette = new Recipe();
+                        blanquette.setName("Blanquette de veau");
+                        blanquette.setDescription("La blanquette, ou blanquette de veau ou blanquette de veau à l\'ancienne, est une recette de cuisine traditionnelle de cuisine française, à base de viande de veau cuite dans un bouillon avec carotte, poireau, oignon et bouquet garni, liée en sauce blanche à la crème et au beurre et aux champignons de Paris.");
+                        blanquette.addIngredient(ingredientsList.get(4), "700 g");
+                        blanquette.addIngredient(ingredientsList.get(3), "50 cl");
+                        blanquette.addIngredient(ingredientsList.get(2), "500 g");
+                        blanquette.setKeywordList(List.of(keywordsList.get(0), keywordsList.get(1), keywordsList.get(2), keywordsList.get(3)));
+                        blanquette.setCreator(adminUser);
+                        recipeRepository.save(blanquette);
+                    }
 
-            // Create Recipe 1
-            r = recipeRepository.findByName("Blanquette de veau");
-            if (r.size() == 0) {
-                Recipe blanquette = new Recipe();
-                blanquette.setName("Blanquette de veau");
-                blanquette.setDescription("La blanquette, ou blanquette de veau ou blanquette de veau à l\'ancienne, est une recette de cuisine traditionnelle de cuisine française, à base de viande de veau cuite dans un bouillon avec carotte, poireau, oignon et bouquet garni, liée en sauce blanche à la crème et au beurre et aux champignons de Paris.");
-                blanquette.addIngredient(ingredientsList.get(4), "700 g");
-                blanquette.addIngredient(ingredientsList.get(3), "50 cl");
-                blanquette.addIngredient(ingredientsList.get(2), "500 g");
-                blanquette.setKeywordList(List.of(keywordsList.get(0),keywordsList.get(1),keywordsList.get(2),keywordsList.get(3)));
-                blanquette.setCreator(adminUser);
-                recipeRepository.save(blanquette);
+                    // Create Recipe 1
+                    r = recipeRepository.findByName("Pot au feu");
+                    if (r.isEmpty()) {
+                        Recipe pot = new Recipe();
+                        pot.setName("Pot au feu");
+                        pot.setDescription("Le pot-au-feu est une recette de cuisine traditionnelle emblématique historique de la cuisine française, et du repas gastronomique des Français, à base de viande de bœuf cuisant longuement à feu très doux dans un bouillon de légumes.");
+                        pot.addIngredient(ingredientsList.get(0), "1kg");
+                        pot.addIngredient(ingredientsList.get(1), "250g");
+                        pot.addIngredient(ingredientsList.get(2), "500g");
+                        pot.setKeywordList(List.of(keywordsList.get(0), keywordsList.get(1), keywordsList.get(2)));
+                        pot.setCreator(adminUser);
+                        recipeRepository.save(pot);
+                    }
+                }
             }
-
-            // Create Recipe 1
-            r = recipeRepository.findByName("Pot au feu");
-            if (r.isEmpty()) {
-                Recipe pot = new Recipe();
-                pot.setName("Pot au feu");
-                pot.setDescription("Le pot-au-feu est une recette de cuisine traditionnelle emblématique historique de la cuisine française, et du repas gastronomique des Français, à base de viande de bœuf cuisant longuement à feu très doux dans un bouillon de légumes.");
-                pot.addIngredient(ingredientsList.get(0), "1kg");
-                pot.addIngredient(ingredientsList.get(1), "250g");
-                pot.addIngredient(ingredientsList.get(2), "500g");
-                pot.setKeywordList(List.of(keywordsList.get(0),keywordsList.get(1),keywordsList.get(2)));
-                pot.setCreator(adminUser);
-                recipeRepository.save(pot);
-            }
-
-
-
-
-
         };
     }
 }
